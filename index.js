8da03af48857a90557c7518790cd8e2429aa80ee
Chris Yip <i@chrisyip.im>
2014-09-17 14:46:18 +0800

    drop stupid require strategy

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 var semver = require('semver'),
     key = '__NODE_EXTENSIONS_VERSION__',
     previousVersion = global[key] || '0.0.0',
-    pkg = require('./package.json'),
-    fs = require('fs')
+    pkg = require('./package.json')
 
 if (semver.gt(pkg.version, previousVersion)) {
-
-  var files = fs.readdirSync('./lib')
-
-  files.forEach(function (file) {
-    var path = './lib/' + file,
-        stats = fs.statSync(path)
-    if (stats.isFile()) {
-      require(path)
-    }
-  })
+  require('./lib/array')
+  require('./lib/boolean')
+  require('./lib/function')
+  require('./lib/object')
+  require('./lib/string')
 
   Object.defineProperty(global, key, {
     configurable: false,
